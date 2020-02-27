e22246eb302a1dddab24c498849f2a664cafabb7
Vlad Dumitrescu <vladdu55@gmail.com>
2020-02-27 19:36:59 +0100

    tailwind 1.2.0 + tw-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,6 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/custom-forms')
+    require('@tailwindcss/ui')
   ]
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,7 @@ let purgecss = require('@fullhuman/postcss-purgecss')({
     ],
 
     // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    defaultExtractor: content => content.match(/[\w-/:\.]+(?<!:)/g) || []
   });
 
 mix.js('source/_assets/js/main.js', outputDir+'/js')
